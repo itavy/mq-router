50616836a159b33208e0e5cf19392fe6bdab182b
Octavian Ionescu <itavyg@gmail.com>
2018-01-08 00:27:33 +0200

    adedd close method for reqeustsRoutingTable
    Signed-off-by: Octavian Ionescu <itavyg@gmail.com>

--- test/RequestsRoutingTable/index.js
+++ test/RequestsRoutingTable/index.js
@@ -2,6 +2,7 @@
 
 describe('RequestsRoutingTable', () => {
   require('./Initialization'); // eslint-disable-line global-require
+  require('./Close'); // eslint-disable-line global-require
   require('./SetMessagesTimeoutListener'); // eslint-disable-line global-require
   require('./Register'); // eslint-disable-line global-require
   require('./CallById'); // eslint-disable-line global-require
